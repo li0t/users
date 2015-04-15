3a332bb83d6ba507b0b48dd703b68b8475468817
li0t <leonardo0ramos@gmail.com>
2015-04-15 15:24:21 -0300

    removed innecessary call to User when adding contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,155 +2,145 @@
 'use strict';
 
 var bcrypt = require('bcrypt'),
-  _ = require('underscore');
+    _ = require('underscore');
 
 module.exports = function (router, mongoose) {
 
-  var Contact = mongoose.model('contact'),
-    User = mongoose.model('user'),
-    Token = mongoose.model('token'),
-    States = {
-      Active: null,
-      Pending: null,
-      Disabled: null
-    };
+    var Contact = mongoose.model('contact'),
+        User = mongoose.model('user'),
+        Token = mongoose.model('token'),
+        States = {
+            Active: null,
+            Pending: null,
+            Disabled: null
+        };
 
-  /** 
-   * Looks for statics states and saves the ids
-   *
-   * FALLS WHEN THERE ARE NO STATICS INSTALLED
-   */
-  (function getStates() {
-    var
-      Sts = mongoose.model('static.state'),
-      state;
+    /** 
+     * Looks for statics states and saves the ids
+     *
+     * FALLS WHEN THERE ARE NO STATICS INSTALLED
+     */
+    (function getStates() {
+        var
+            Sts = mongoose.model('static.state'),
+            state;
 
-    function lookup(name) {
-      Sts.find({
-        name: name
-      }, function (err, result) {
-        if (err) {
-          console.log(err);
-        } else {
-          States[name] = result[0]._id;
-        }
-      });
-    }
-
-    for (state in States) {
-      if (States.hasOwnProperty(state)) {
-        lookup(state);
-      }
-    }
-
-  })();
-
-
-  /** 
-   * Add contact
-   */
-  router.get('/add/:id', function (req, res, next) {
-    User.findById(req.params.id, function (err, user) { /* The user recieving the request */
-      if (err) {
-        next(err);
-      } else if (user) {
-        Contact.find().where('user', user._id).exec(function (err, receiver) { /* The ContactSchema of the receiver */
-          if (err) {
-            next(err)
-          } else {
-            if (receiver) {
-              Contact.find().where('user', req.session.user._id).exec(function (err, sender) { /* The ContactSchema of the sender */
+        function lookup(name) {
+            Sts.find({
+                name: name
+            }, function (err, result) {
                 if (err) {
-                  next(err)
+                    console.log(err);
                 } else {
-                  if (sender) {
-                    sender[0].contacts.push({ /* Pushes the receiver id into the sender contacts */
-                      user: user._id,
-                      state: States.Pending
-                    });
-                    receiver[0].contacts.push({ /* Pushes the sender id into the receiver contacts */
-                      user: req.session.user._id,
-                      state: States.Pending
-                    });
-                    sender[0].save(function (err) {
-                      if (err) {
-                        next(err);
-                      } else {
-                        receiver[0].save(function (err) {
-                          if (err) {
-                            next(err);
-                          } else {
-                            //res.redirect('/api/notifications/pending/' + user._id); /* if done, redirects to notifications api */
-                            res.redirect('/api/mandrill/addContact/' + user._id);
-                          }
-                        });
-                      }
-                    });
-                  } else {
-                    res.status(404).end();
-                  }
+                    States[name] = result[0]._id;
                 }
-              });
-            } else {
-              res.status(404).end();
-            }
-          }
-        });
-      } else {
-        res.status(404).end();
-      }
-    });
-  });
+            });
+        }
 
-  /** 
-   *  Confirm request
-   */
-  router.get('/confirm/:id', function (req, res, next) {
-    Contact.find().where('user', req.params.id).exec(function (err, sender) {
-      if (err) {
-        next(err);
-      } else if (sender) {
-        Contact.find().where('user', req.session.user._id).exec(function (err, receiver) {
-          if (err) {
-            next(err);
-          } else if (receiver) {
-            for (var i = 0; i < sender[0].contacts.length; i++) {
-              if (JSON.stringify(sender[0].contacts[i].user) === JSON.stringify(req.session.user._id)) {
-                sender[0].contacts[i].state = States.Active;
-                break;
-              }
+        for (state in States) {
+            if (States.hasOwnProperty(state)) {
+                lookup(state);
             }
-            for (i = 0; i < receiver[0].contacts.length; i++) {
-              if (JSON.stringify(receiver[0].contacts[i].user) === JSON.stringify(req.params.id)) {
-                receiver[0].contacts[i].state = States.Active;
-                break;
-              }
-            }
-            sender[0].save(function (err) {
-              if (err) {
+        }
+
+    })();
+
+
+    /** 
+     * Add contact
+     */
+    router.get('/add/:id', function (req, res, next) {
+        Contact.find()
+            .where('user', req.params.id)
+            .exec(function (err, receiver) { /* The ContactSchema of the receiver */
+                if (err) {
+                    next(err);
+                } else if (receiver) {
+                    Contact.find().where('user', req.session.user._id).exec(function (err, sender) { /* The ContactSchema of the sender */
+                        if (err) {
+                            next(err);
+                        } else if (sender) {
+                            sender[0].contacts.push({ /* Push the receiver id into the sender contacts */
+                                user: req.params.id,
+                                state: States.Pending
+                            });
+                            receiver[0].contacts.push({ /* Push the sender id into the receiver contacts */
+                                user: req.session.user._id,
+                                state: States.Pending
+                            });
+                            sender[0].save(function (err) {
+                                if (err) {
+                                    next(err);
+                                } else {
+                                    receiver[0].save(function (err) {
+                                        if (err) {
+                                            next(err);
+                                        } else {
+                                            res.redirect('/api/mandrill/addContact/' + req.params.id); /* Redirect to email manager */
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            res.status(404).end();
+                        }
+
+                    });
+                } else {
+                    res.status(404).end();
+                }
+            });
+    });
+
+    /** 
+     *  Confirm request
+     */
+    router.get(':receiverId/confirm/:senderId', function (req, res, next) {
+        Contact.find().where('user', req.params.id).exec(function (err, sender) {
+            if (err) {
                 next(err);
-              } else {
-                receiver[0].save(function (err) {
-                  if (err) {
-                    next(err)
-                  } else {
-                    res.status(200).send(receiver);
-                  }
+            } else if (sender) {
+                Contact.find().where('user', req.session.user._id).exec(function (err, receiver) {
+                    if (err) {
+                        next(err);
+                    } else if (receiver) {
+                        for (var i = 0; i < sender[0].contacts.length; i++) {
+                            if (JSON.stringify(sender[0].contacts[i].user) === JSON.stringify(req.session.user._id)) {
+                                sender[0].contacts[i].state = States.Active;
+                                break;
+                            }
+                        }
+                        for (i = 0; i < receiver[0].contacts.length; i++) {
+                            if (JSON.stringify(receiver[0].contacts[i].user) === JSON.stringify(req.params.id)) {
+                                receiver[0].contacts[i].state = States.Active;
+                                break;
+                            }
+                        }
+                        sender[0].save(function (err) {
+                            if (err) {
+                                next(err);
+                            } else {
+                                receiver[0].save(function (err) {
+                                    if (err) {
+                                        next(err);
+                                    } else {
+                                        res.redirect('/api/users/' + req.session.user._id);
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        res.status(404).end();
+                    }
                 });
-              }
-            })
-          } else {
-            res.status(404).end();
-          }
+            } else {
+                res.status(404).end();
+            }
         });
-      } else {
-        res.status(404).end();
-      }
     });
-  });
 
-  router.get('/delete/:id', function (req, res, next) {
-    /*TODO*/
-  });
+    router.get('/delete/:id', function (req, res, next) {
+        /*TODO*/
+    });
 
-}
+};
