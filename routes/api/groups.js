a1c7dd6f60aa65e7ddee291f26e3c950c59fe584
li0t <leonardo0ramos@gmail.com>
2015-04-24 17:52:32 -0300

    Added group api with basic functionality

---
+++ routes/api/groups.js
@@ -0,0 +1,235 @@
+/* jshint node: true */
+'use strict';
+
+var    _ = require('underscore'),
+    debug = require('debug')('app:api:groups');
+
+
+module.exports = function (router, mongoose) {
+
+  var Group = mongoose.model('group'),
+      User = mongoose.model('user'),
+      Profile = mongoose.model('profile');
+
+
+  /**
+   * Create new group
+   */
+  router.post('/create', function(req, res, next) {
+
+    new Profile({
+      name : req.body.name
+    }).
+
+    save(function(err, profile){
+
+      if(err) {
+        next(err);
+      } else {
+
+        new Group({
+          profile : profile._id,
+          admin : req.session.user._id
+        }).
+
+        save(function(err, group){
+
+          if (err) {
+            next(err);
+          } else {
+
+            debug('Saved group with id %s' , group._id);
+            res.status(201).send(group._id);
+
+          }
+        });
+      }
+    });
+
+  });
+
+  /**
+   * Add member to a group
+   */
+  router.get('/:groupId/addMember/:id', function(req, res, next) {
+
+    Group.findById(req.params.groupId, function(err, group) {
+
+      if (err) {
+        next(err);
+      } else if (group) {
+
+        if(_.isEqual(group.admin, req.session.user._id)) { /** Check if logged user is the group admin */
+
+          User.findById(req.params.id, function(err, user) {
+
+            if (err) {
+              next(err);
+            } else if (user) {
+
+              group.members.push(user._id);
+
+              group.save(function(err) {
+
+                if (err) {
+                  next(err);
+                } else {
+
+                  debug('User %s added to group %s' , group._id, user._id);
+                  res.sendStatus(204);
+
+                }
+              });
+            } else {
+              debug('No user found with id %s' , req.params.id);
+              res.sendStatus(404);
+            }
+          });
+        } else {
+          res.sendStatus(403);
+        }
+      } else {
+        debug('No group found with id %s' , req.params.gruopId);
+        res.sendStatus(404);
+      }
+    });
+
+  });
+
+  /**
+   * Remove member from group
+   */
+  router.get('/:groupId/removeMember/:id', function(req, res, next) {
+
+    var i, index = -1;
+
+    Group.findById(req.params.groupId, function(err, group) {
+
+      if (err) {
+        next(err);
+      } else if (group) {
+        
+        /** Check if the user removing is the group admin or itself */
+        if (_.isEqual(group.admin, req.session.user._id) || _.isEqual(req.session.user._id, req.params.id)) { 
+
+          for (i = 0; i < group.members.length; i++) {
+
+            if (_.isEqual(group.members[i], req.params.id)) { /** Look for user index in members array */
+              index = i;
+              break;
+            }
+          }
+
+          if (index > -1) { /** Check if user was found */
+
+            group.members.splice(index, 1); /** Remove user from members array */
+
+            group.save(function(err) {
+
+              if (err) {
+                next(err);
+              } else {
+
+                debug('User %s removed from group %s' , group._id, req.params.id);
+                res.sendStatus(204);
+
+              }
+            });
+          } else {
+            debug('No user with id %s found in group %s' , req.params.id, req.params.gruopId);
+            res.sendStatus(404);
+          }
+        } else {
+          res.sendStatus(403);
+        }
+      } else {
+        debug('No group found with id %s' , req.params.gruopId);
+        res.sendStatus(404);
+      }
+    });
+
+  });
+
+  /**
+   * Change group admin
+   */
+  router.get('/:groupId/changeAdmin/:id', function(req, res, next) {
+
+    var i, index = -1;
+
+    Group.findById(req.params.groupId, function(err, group) {
+
+      if (err) {
+        next(err);
+      } else if (group) {
+
+        if (_.isEqual(group.admin, req.session.user._id)) { /** Check if logged user is the group admin */
+
+          for (i = 0; i < group.members.length; i++) {
+
+            if (_.isEqual(group.members[i], req.params.id)) { /** Look for user index in members array */
+              index = i;
+              break;
+            }
+          }
+
+          if (index > -1) { /** Check if user was found */
+
+            group.admin = req.params.id;
+
+            group.save(function(err) {
+
+              if (err) {
+                next(err);
+              } else {
+
+                debug('The group %s, has a new admin with id %s' , group._id, req.params.id);
+                res.sendStatus(204);
+
+              }
+            });
+          } else {
+            debug('No user with id %s found in group %s' , req.params.id, req.params.gruopId);
+            res.sendStatus(404);
+          }
+        } else {
+          res.sendStatus(403);
+        }
+      } else {
+        debug('No group found with id %s' , req.params.gruopId);
+        res.sendStatus(404);
+      }
+    });
+
+  });
+  
+  /**
+   * Get group members
+   */
+  router.get(':groupId/members', function(req, res, next) {
+
+    Group.
+    
+    findOne().
+    where('_id', req.params.id).
+    where('members', req.session.user._id).
+    
+    deepPopulate('members.email members.profile').
+    
+    exec(function(err, group){
+      
+      if (err) {
+        next(err);
+      } else if (group) {
+        
+        res.send(group);
+        
+      } else {
+        res.sendStatus(404);
+      }
+    });
+
+  });
+
+
+};
