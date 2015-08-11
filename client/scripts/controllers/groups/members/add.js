5b9cdd577cc3525a4ce062dc451cc6e8cd2f4f9a
li0t <leonardo0ramos@gmail.com>
2015-08-11 12:43:35 -0300

    Add members to a Group

--- client/scripts/controllers/groups/members/add.js
+++ client/scripts/controllers/groups/members/add.js
@@ -7,10 +7,23 @@
     function ($scope, $http, $location, $session, $routeParams) {
 
       $scope.fetching = false;
-
       $scope.data = {
         members: []
       };
+
+      function filterContacts(groupMembers) {
+        var i, j;
+
+        for (i = 0; i < $scope.contacts.length; i++) {
+          for (j = 0; j < groupMembers.length; j++) {
+            if ($scope.contacts[i].user.email === groupMembers[j].user.email) {
+              $scope.contacts.splice(i, 1);
+              i -= 1;
+              break;
+            }
+          }
+        }
+      }
 
       $scope.fetch = function () {
 
@@ -20,10 +33,16 @@
 
         success(function(data) {
           $scope.contacts = data;
-        }).
 
-        finally(function(){
-          $scope.fetching = false;
+          $http.get('/api/groups/members/of/' + $session.get('group')._id).
+
+          success(function(data) {
+            filterContacts(data);
+          }).
+
+          finally(function(){
+            $scope.fetching = false;
+          });
         });
       };
 
@@ -37,8 +56,8 @@
             this.list.push(item);
           }
 
-          if ($scope.data.members.indexOf(item.id) < 0) {
-            $scope.data.members.push(item.id);
+          if ($scope.data.members.indexOf(item.user._id) < 0) {
+            $scope.data.members.push(item.user._id);
           }
 
           $scope.form.member = '';
@@ -54,7 +73,7 @@
         $http.post('/api/groups/members/add-to/' + $routeParams.id, $scope.data).
         success(function () {
           $session.flash('Miembros agregados');
-          $location.path('/groups/' + $session.get('group')._id+ '/members');
+          $location.path('/groups/' + $session.get('group')._id + '/members');
         }).
         error(function () {
           $session.flash('Hubo un error agregando miembros');
