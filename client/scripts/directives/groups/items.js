fa456babbc98aed1b093eea20eff3e5cad5d2966
li0t <leonardo0ramos@gmail.com>
2015-08-18 12:43:20 -0300

    Add group-items directive

---
+++ client/scripts/directives/groups/items.js
@@ -0,0 +1,22 @@
+/**
+ * Groups Items Directive.
+ *
+ * @type AngularJS Directive.
+ */
+
+(function (ng) {
+  'use strict';
+
+  ng.module('App').directive('groupItems', [
+
+    function () {
+
+      return {
+        restrict : 'E',
+        templateUrl: '/assets/templates/groups/items.html'
+      };
+    }
+
+  ]);
+
+}(angular));
