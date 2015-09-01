0b5869e020d319e50db94b198049937be6a677ea
li0t <leonardo0ramos@gmail.com>
2015-09-01 16:43:04 -0300

    Add em-card detail button

--- client/scripts/controllers/entries/create/index.js
+++ client/scripts/controllers/entries/create/index.js
@@ -10,19 +10,15 @@
       [{
         title: 'Imagen',
         type: 'image',
-        href: '/entries/create/image'
       }, {
         title: 'Audio',
         type: 'audio',
-        href: '/entries/create/audio'
       }, {
         title: 'Nota',
         type: 'note',
-        href: '/entries/create/note'
       }, {
         title: 'Documento',
         type: 'document',
-        href: '/entries/create/document'
       }];
 
     }
--- client/scripts/directives/main/em-card.js
+++ client/scripts/directives/main/em-card.js
@@ -32,6 +32,7 @@
               color: "#f34235",
               span: 'Nueva Nota',
               background: 'yellow',
+              href: ($scope.card._id && '/entries/' + $scope.card._id + '/detail') || '/entries/create/note',
               footer: $scope.card.user && $scope.card.user.email + ' ha creado una nota.'
             },
             "document": {
@@ -39,6 +40,7 @@
               color: "#ffc006",
               span: 'Nuevo Documento',
               background: 'yellow',
+              href:  ($scope.card._id &&  '/entries/' + $scope.card._id + '/detail') || '/entries/create/document',
               footer: $scope.card.user && $scope.card.user.email + ' ha agregado un documento.'
             },
             "image": {
@@ -46,6 +48,7 @@
               color: "#3e50b4",
               span: 'Nueva Imagen',
               background: 'yellow',
+              href:  ($scope.card._id &&  '/entries/' + $scope.card._id + '/detail') || '/entries/create/image',
               footer: $scope.card.user && $scope.card.user.email + ' ha subido una imagen.'
             },
             "audio": {
@@ -53,6 +56,7 @@
               color: "#00bbd3",
               span: 'Nuevo Audio',
               background: 'yellow',
+              href:  ($scope.card._id &&  '/entries/' + $scope.card._id + '/detail') || '/entries/create/audio',
               footer: $scope.card.user && $scope.card.user.email + ' ha subido un audio.'
             },
             "group": {
@@ -60,6 +64,7 @@
               color: "#02A8F3",
               span: "Grupo creado",
               background: "green",
+              href: '/groups/' + $scope.card._id + '/profile',
               footer: $scope.card.admin && $scope.card.admin.email + ' ha creado el grupo.'
             },
             "task": {
@@ -67,6 +72,7 @@
               color: "#9b26af",
               span: "Tarea modificada",
               background: "lightBlue",
+              href: '/tasks/' + $scope.card._id + '/detail',
               footer: $scope.card.creator && $scope.card.creator.email + ' ha creado una tarea.'
             },
             "meeting": {
@@ -74,6 +80,7 @@
               color: "#8ac248",
               span: "Nueva reunión",
               background: "green",
+              href: '/meetings/' + $scope.card._id + '/detail',
               footer: $scope.card.creator && $scope.card.creator.email + ' ha agendado una reunión.'
             }
           };
@@ -84,6 +91,7 @@
           $scope.card.color = config[$scope.card.type].color;
           $scope.card.icon = config[$scope.card.type].icon;
           $scope.card.span = config[$scope.card.type].span;
+          $scope.card.href = config[$scope.card.type].href;
           $scope.card.relevantDate = $scope.card.created;
 
 
@@ -121,7 +129,7 @@
               break;
           }
 
-          $scope.card.action = function() {
+          $scope.card.action = $scope.card._id && function() {
             $emCard.setCard($scope.card);
             $emCard.showDetailsBar(true);
           };
