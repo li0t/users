1210cf878770b7fc7a0e93828dd5b154f80b916f
li0t <leonardo0ramos@gmail.com>
2015-09-15 15:44:13 -0300

    Advance in analytics example

--- client/scripts/directives/main/d3-example.js
+++ client/scripts/directives/main/d3-example.js
@@ -17,26 +17,44 @@
         restrict: 'E',
         templateUrl: '/assets/templates/main/d3example.html',
         link: function($scope) {
-          
-          var data = [4, 8, 15, 16, 23, 42];
 
-          var
-            scale = d3.scale.linear()
-            .domain([0, d3.max(data)])
-            .range([0, 420]);
+          var color, pack, svg;
 
-          d3.
-          select('.chart').
-          selectAll('div').
-          data(data).
-          enter().
-          append('div').
-          style('width', function(d) {
-            return scale(d) + 'px';
-          }).text(function(d) {
-            return d;
+          var diameter = 960;
+          var margin = 20;
+
+          color = d3.scale.
+          linear().
+          domain([-1, 5]).
+          range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"]).
+          interpolate(d3.interpolateHcl);
+
+          pack = d3.layout.pack().
+          padding(2).
+          size([diameter - margin, diameter - margin]).
+          value(function(d) {
+            return d.size;
           });
 
+          svg = d3.select(".content").
+          append("svg").
+          attr("width", diameter).
+          attr("height", diameter).
+          append("g").
+          attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+          d3.json("/api/analytics/groups", function(error, root) {
+            if (error) throw error;
+console.log(root);
+            var circle, text, node, transition, view;
+
+            var focus = root;
+            var nodes = pack.nodes(root);
+console.log(nodes);
+
+          });
+
+          d3.select(self.frameElement).style("height", diameter + "px");
         }
       };
     }
