d9f2058552ac4a86ee9fece416b953a4833f53b4
li0t <leonardo0ramos@gmail.com>
2015-09-23 17:24:17 -0300

    Return normalized data to worked-times directive

--- client/scripts/directives/main/worked-times.js
+++ client/scripts/directives/main/worked-times.js
@@ -9,187 +9,175 @@
 
   ng.module('App').directive('workedTimes', [
 
+    '$moment',
     '$http',
 
-    function($http) {
+    function($moment, $http) {
 
       return {
         restrict: 'E',
         templateUrl: '/assets/templates/main/worked-times.html',
         link: function($scope) {
 
-          var layers, yGroupMax, yStackMax, margin, color, x, y;
-          var xAxis, svg, layer, rect, width, height, timeout;
+          var layers, yGroupMax, yStackMax, margin, color, task;
+          var xAxis, svg, layer, rect, width, height, timeout, x, y;
 
           var n = 4; // number of layers
           var m = 58; // number of samples per layer
-          var stack = d3.layout.stack();
+          var stack = d3.layout.stack().values(function(d) { return d.values; });
 
-          layers = stack(d3.range(n).map(function() {
-            return bumpLayer(m, 0.1);
-          }));
 
-          yGroupMax = d3.max(layers, function(layer) {
-            return d3.max(layer, function(d) {
-              return d.y;
-            });
-          });
+          $http.get('api/tasks/collaborators/me').success(function(tasks) {
+            if (!tasks.length) return;
 
-          yStackMax = d3.max(layers, function(layer) {
-            return d3.max(layer, function(d) {
-              return d.y0 + d.y;
-            });
-          });
+            task = tasks[Math.round(Math.random() * (tasks.length - 1))]._id;
 
-          margin = {
-            bottom: 20,
-            right: 10,
-            left: 10,
-            top: 40
-          };
+            $http.get('api/analytics/worked-times/of/' + task).
 
-          width = 960 - margin.left - margin.right;
-          height = 500 - margin.top - margin.bottom;
+            success(function(data) {
+              console.log(data);
 
-          x = d3.scale.
-          ordinal().
-          domain(d3.range(m)).
-          rangeRoundBands([0, width], 0.08);
+              layers = stack(data);
 
-          y = d3.scale.
-          linear().
-          domain([0, yStackMax]).
-          range([height, 0]);
+              yGroupMax = d3.max(layers, function(layer) {
+                return d3.max(layer, function(d) {
+                  return d.y;
+                });
+              });
 
-          color = d3.scale.
-          linear().domain([0, n - 1]).
-          range(["#aad", "#556"]);
+              yStackMax = d3.max(layers, function(layer) {
+                return d3.max(layer, function(d) {
+                  return d.y0 + d.y;
+                });
+              });
 
-          xAxis = d3.svg.axis().
-          scale(x).
-          tickSize(0).
-          tickPadding(6).
-          orient("bottom");
-
-          svg = d3.select(".times").
-          append("svg").
-          attr("width", width + margin.left + margin.right).
-          attr("height", height + margin.top + margin.bottom).
-          append("g").
-          attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          layer = svg.selectAll(".layer").
-          data(layers).
-          enter().append("g").
-          attr("class", "layer").
-          style("fill", function(d, i) {
-            return color(i);
-          });
-
-          rect = layer.selectAll("rect").
-          data(function(d) {
-            return d;
-          }).
-          enter().append("rect").
-          attr("x", function(d) {
-            return x(d.x);
-          }).
-          attr("y", height).
-          attr("width", x.rangeBand()).
-          attr("height", 0);
-
-          rect.transition().
-          delay(function(d, i) {
-            return i * 10;
-          }).
-          attr("y", function(d) {
-            return y(d.y0 + d.y);
-          }).
-          attr("height", function(d) {
-            return y(d.y0) - y(d.y0 + d.y);
-          });
-
-          svg.append("g").
-          attr("class", "x axis").
-          attr("transform", "translate(0," + height + ")").
-          call(xAxis);
-
-          d3.selectAll("input").on("change", change);
-
-          function change() {
-            /*jshint validthis:true */
-
-            if (this.value === "grouped")
-              transitionGrouped();
-            else
-              transitionStacked();
-
-          }
-
-          function transitionGrouped() {
-            y.domain([0, yGroupMax]);
-
-            rect.transition().
-            duration(500).
-            delay(function(d, i) {
-              return i * 10;
-            }).
-            attr("x", function(d, i, j) {
-              return x(d.x) + x.rangeBand() / n * j;
-            }).
-            attr("width", x.rangeBand() / n).transition().attr("y", function(d) {
-              return y(d.y);
-            }).
-            attr("height", function(d) {
-              return height - y(d.y);
-            });
-          }
-
-          function transitionStacked() {
-            y.domain([0, yStackMax]);
-
-            rect.transition().
-            duration(500).
-            delay(function(d, i) {
-              return i * 10;
-            }).
-            attr("y", function(d) {
-              return y(d.y0 + d.y);
-            }).
-            attr("height", function(d) {
-              return y(d.y0) - y(d.y0 + d.y);
-            }).
-            transition().attr("x", function(d) {
-              return x(d.x);
-            }).
-            attr("width", x.rangeBand());
-          }
-
-          // Inspired by Lee Byron's test data generator.
-          function bumpLayer(n, o) {
-
-            function bump(a) {
-              var x = 1 / (0.1 + Math.random()),
-                y = 2 * Math.random() - 0.5,
-                z = 10 / (0.1 + Math.random());
-              for (var i = 0; i < n; i++) {
-                var w = (i / n - y) * z;
-                a[i] += x * Math.exp(-w * w);
-              }
-            }
-
-            var a = [],
-              i;
-            for (i = 0; i < n; ++i) a[i] = o + o * Math.random();
-            for (i = 0; i < 5; ++i) bump(a);
-            return a.map(function(d, i) {
-              return {
-                x: i,
-                y: Math.max(0, d)
+              margin = {
+                bottom: 20,
+                right: 10,
+                left: 10,
+                top: 40
               };
-            });
-          }
 
+              width = 960 - margin.left - margin.right;
+              height = 500 - margin.top - margin.bottom;
+
+              x = d3.scale.
+              ordinal().
+              domain(d3.range(m)).
+              rangeRoundBands([0, width], 0.08);
+
+              y = d3.scale.
+              linear().
+              domain([0, yStackMax]).
+              range([height, 0]);
+
+              color = d3.scale.
+              linear().domain([0, n - 1]).
+              range(["#aad", "#556"]);
+
+              xAxis = d3.svg.axis().
+              scale(x).
+              tickSize(0).
+              tickPadding(6).
+              orient("bottom");
+
+              svg = d3.select(".times").
+              append("svg").
+              attr("width", width + margin.left + margin.right).
+              attr("height", height + margin.top + margin.bottom).
+              append("g").
+              attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+              layer = svg.selectAll(".layer").
+              data(layers).
+              enter().append("g").
+              attr("class", "layer").
+              style("fill", function(d, i) {
+                return color(i);
+              });
+
+              rect = layer.selectAll("rect").
+              data(function(d) {
+                return d;
+              }).
+              enter().append("rect").
+              attr("x", function(d) {
+                return x(d.x);
+              }).
+              attr("y", height).
+              attr("width", x.rangeBand()).
+              attr("height", 0);
+
+              rect.transition().
+              delay(function(d, i) {
+                return i * 10;
+              }).
+              attr("y", function(d) {
+                return y(d.y0 + d.y);
+              }).
+              attr("height", function(d) {
+                return y(d.y0) - y(d.y0 + d.y);
+              });
+
+              svg.append("g").
+              attr("class", "x axis").
+              attr("transform", "translate(0," + height + ")").
+              call(xAxis);
+
+              d3.selectAll("input").on("change", change);
+
+              function change() {
+                /*jshint validthis:true */
+
+                if (this.value === "grouped")
+                  transitionGrouped();
+                else
+                  transitionStacked();
+
+              }
+
+              function transitionGrouped() {
+                y.domain([0, yGroupMax]);
+
+                rect.transition().
+                duration(500).
+                delay(function(d, i) {
+                  return i * 10;
+                }).
+                attr("x", function(d, i, j) {
+                  return x(d.x) + x.rangeBand() / n * j;
+                }).
+                attr("width", x.rangeBand() / n).transition().
+                attr("y", function(d) {
+                  return y(d.y);
+                }).
+                attr("height", function(d) {
+                  return height - y(d.y);
+                });
+              }
+
+              function transitionStacked() {
+                y.domain([0, yStackMax]);
+
+                rect.transition().
+                duration(500).
+                delay(function(d, i) {
+                  return i * 10;
+                }).
+                attr("y", function(d) {
+                  return y(d.y0 + d.y);
+                }).
+                attr("height", function(d) {
+                  return y(d.y0) - y(d.y0 + d.y);
+                }).
+                transition().
+                attr("x", function(d) {
+                  return x(d.x);
+                }).
+                attr("width", x.rangeBand());
+              }
+            });
+          });
         }
       };
     }
--- server/routes/api/analytics.js
+++ server/routes/api/analytics.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var debug = require('debug')('app:api:analytics');
+var moment = require('moment');
 
-// var relations = component('relations');
+var relations = component('relations');
 
 module.exports = function(router, mongoose) {
 
@@ -194,6 +195,114 @@ module.exports = function(router, mongoose) {
 
     });
 
+  });
+
+  router.get('/worked-times/of/:id', function(req, res, next) {
+
+    var user = req.session.user._id;
+    var task = req.params.id;
+    var data = [];
+    var i, allDays = [];
+
+
+    /**
+     * Returns an object where each key has the same amount of days worked
+     * where the minimun time worked is 0 milliseconds
+     **/
+    function normalizeData() {
+      var present;
+
+      data.forEach(function(collaborator) {
+        allDays.forEach(function(day) {
+
+          present = collaborator.values.filter(function(span) {
+            return day === span.x;
+          });
+
+          if (!present.length) {
+            collaborator.values.push({ x: day, y: 0 });
+
+          }
+        });
+      });
+    }
+
+    /** Return an array of milliseconds worked per day */
+    function workedTimes(times) {
+
+      var span;
+      var current = times.length && moment(times[0]);
+      var days = current && [{
+        x: current.dayOfYear(),
+        y: current.millisecond()
+      }];
+
+      for (i = 1; i < times.length; i++) {
+        span = moment(times[i]);
+
+        if (allDays.indexOf(current.dayOfYear()) < 0) {
+          allDays.push(current.dayOfYear());
+        }
+
+        if (current.diff(span, 'days')) {
+          current = span;
+          days.push({
+            x: current.dayOfYear(),
+            y: current.millisecond()
+          });
+
+        } else {
+
+          days[days.length - 1].y += span.millisecond();
+        }
+
+
+      }
+
+      return days;
+
+    }
+
+    relations.collaboration(task, function(err, collaboration) {
+
+      /** Check if task exists and is available for changes */
+      if (err || !collaboration.task) {
+        debug('Task %s was not found', req.params.id);
+        return res.sendStatus(404);
+      }
+
+      task = collaboration.task; /** The task model */
+
+      relations.membership(task.group, function(err, taskGroup) {
+
+        if (err || !taskGroup.group || !taskGroup.isMember(user)) { /** Check if user is part of the task group */
+          debug('User %s is not part of the task %s group', user, task._id);
+          return res.sendStatus(403);
+        }
+
+        task.deepPopulate('collaborators.user', function(err, task) {
+          if (err) {
+            return next(err);
+          }
+
+          task.collaborators.forEach(function(collaborator) {
+
+            /** Check if user is actual collaborator of task */
+            if (!collaborator.left.length || (collaborator.left.length < collaborator.joined.length)) {
+              data.push({
+                "name": collaborator.user.email,
+                values: workedTimes(collaborator.workedTimes)
+              });
+            }
+          });
+
+          normalizeData();
+
+          res.send(data);
+
+        });
+      });
+    });
   });
 
 };
